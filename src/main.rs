349428b5b06d49dae32086a5a6bdb6d997ccc033
Ben Wormald <benwormald95@gmail.com>
2021-11-05 21:02:42 +0000

    add source text, play with str bytes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+#[allow(dead_code)]
+const SOURCE: &str = "./text/paradise-lost.txt";
+#[allow(dead_code)]
+const TEST: &str = "./text/test.txt";
+
 fn main() {
-    println!("Hello, world!");
+    let source = File::open(TEST).unwrap();
+    let reader = BufReader::new(source);
+
+    for line in reader.lines() {
+        let line = process_line(&line.unwrap());
+        println!("{}", line);
+    }
+}
+
+fn process_line(line: &str) -> String {
+    let bytes = line.bytes();
+    let bytes = bytes.map(|byte| byte + 1).collect();
+    String::from_utf8(bytes).unwrap()
 }
