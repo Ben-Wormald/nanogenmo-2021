84f10f778ce38c82d1ba40ae2e8337c367b21054
Ben Wormald <benwormald95@gmail.com>
2021-11-05 21:26:55 +0000

    write output

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use std::fs::File;
-use std::io::{prelude::*, BufReader};
+use std::io::{prelude::*, BufReader, BufWriter};
 
 #[allow(dead_code)]
 const SOURCE: &str = "./text/paradise-lost.txt";
 #[allow(dead_code)]
 const TEST: &str = "./text/test.txt";
+const OUTPUT: &str = "./text/out.txt";
 
 fn main() {
-    let source = File::open(TEST).unwrap();
+    let source = SOURCE;
+    let source = File::open(source).expect("couldn't read source");
     let reader = BufReader::new(source);
+
+    let output = File::create(OUTPUT).expect("couldn't create output");
+    let mut writer = BufWriter::new(output);
 
     for line in reader.lines() {
         let line = process_line(&line.unwrap());
-        println!("{}", line);
+        writer.write(line.as_bytes()).unwrap();
+        writer.write("\n".as_bytes()).unwrap();
     }
 }
 
