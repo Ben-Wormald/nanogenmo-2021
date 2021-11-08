8dc60b9436b44e110e04a240554ce03934396a3e
Ben Wormald <benwormald95@gmail.com>
2021-11-08 21:00:34 +0000

    flip random bits with chance

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,68 @@
+use std::env;
 use std::fs::{File, metadata};
 use std::io::{prelude::*, BufReader, BufWriter};
+use rand::prelude::*;
 
-#[allow(dead_code)]
-const SOURCE: &str = "./text/paradise-lost.txt";
-#[allow(dead_code)]
-const TEST: &str = "./text/test.txt";
-const OUTPUT: &str = "./text/out.txt";
+const INTRO: &str = "./text/intro.txt";
+const OUTPUT: &str = "./text/with-loss-of-eden.txt";
+
+const BITS: [u8; 8] = [1, 2, 4, 8, 16, 32, 64, 128];
 
 fn main() {
-    let source = TEST;
-    let total_bytes = metadata(&source).unwrap().len();
+    let args: Vec<String> = env::args().collect();
 
-    let source = File::open(source).expect("couldn't read source");
-    let reader = BufReader::new(source);
+    let sources = vec!(
+        INTRO,
+        &args[1]
+    );
+
+    let total_bytes = sources.iter().fold(0, |bytes, source|
+        bytes + metadata(source).unwrap().len()
+    );
+
+    let mut bytes_read = 0;
 
     let output = File::create(OUTPUT).expect("couldn't create output");
     let mut writer = BufWriter::new(output);
 
-    let mut bytes_read = 0;
+    let mut rng = thread_rng();
+
+    sources.iter().for_each(|source|
+        process_source(source, &mut bytes_read, &total_bytes, &mut writer, &mut rng)
+    );
+}
+
+fn process_source(
+    source: &str,
+    bytes_read: &mut u64,
+    total_bytes: &u64,
+    writer: &mut BufWriter<File>,
+    rng: &mut ThreadRng,
+) {
+    let source = File::open(source).expect(&format!("couldn't read source {}", source));
+    let reader = BufReader::new(source);
 
     for line in reader.lines() {
-        let line = process_line(&line.unwrap(), &bytes_read, &total_bytes);
-        bytes_read += line.len() as u64 + 1;
+        let line = process_line(&line.unwrap(), &bytes_read, &total_bytes, rng);
+        *bytes_read += line.len() as u64 + 1;
         writer.write(&line).unwrap();
         writer.write("\n".as_bytes()).unwrap();
     }
 }
 
-fn process_line(line: &str, bytes_read: &u64, total_bytes: &u64) -> Vec<u8> {
+fn process_line(
+    line: &str,
+    bytes_read: &u64,
+    total_bytes: &u64,
+    rng: &mut ThreadRng,
+) -> Vec<u8> {
     let bytes = line.bytes();
-    let offset = *bytes_read as f64 / *total_bytes as f64;
-    let offset = offset.round() as u8;
-    bytes.map(|byte| byte + offset).collect()
+    let chance = *bytes_read as f64 / *total_bytes as f64;
+
+    bytes.map(|byte|
+        BITS.iter().fold(byte, |byte, bit| {
+            let should_flip: f64 = rng.gen();
+            if should_flip < chance { byte ^ bit } else { byte }
+        })
+    ).collect()
 }
